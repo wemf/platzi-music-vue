6f0f511c41954b76cae336462a513a19ee1e37d5
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-06 16:43:40 -0500

    list values

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
     h1 {{ msg }}
+    p {{ person.name }}
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="true") {{ 'algo mas' }}
+    p(v-else) {{ 'lo ultimo' }}
+
+    ul
+      li(v-for="item in items") {{ item }}
 </template>
 
 <script>
@@ -9,7 +16,13 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Vue!'
+      msg: 'Hola Vue!',
+      person: {
+        name: 'William'
+      },
+      showValue: true,
+      value: 'Algo',
+      items: [1, 2, 3, 4, 5]
     }
   }
 }
