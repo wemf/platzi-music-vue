80a218f0c2f73b1643789c49a8ac08a594868b19
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-07 09:29:51 -0500

    Events

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,26 @@
 <template lang="pug">
   #app
     label(for="name") Nombre
-    input(v-model="name" id="name")
-    label(for="lastName") Apellido
-    input(v-model="lastName" id="lastName")
-    label(for="birthyear") Año de Nacimiento
-    input(type="number" v-model="birthyear" id="birthyear")
-    label(for="birthmonth") Mes de Nacimiento
-    input(type="number" v-model="birthmonth" id="birthmonth")
-    label(for="birthday") Dia de Nacimiento
-    input(type="number" v-model="birthday" id="birthday")
-    p {{ fullName }}
-    p {{ age }}
+    input(v-model="name" id="name" name="name")
+    button(v-on:click="format") Formatear Nombre
+
+    p {{ formattedName }}
 </template>
 
 <script>
 export default {
   name: 'app',
+
   data () {
     return {
       name: '',
-      lastName: '',
-      birthyear: '',
-      birthmonth: '',
-      birthday: ''
+      formattedName: ''
     }
   },
 
-  computed: {
-    fullName () {
-      return `${this.name} ${this.lastName}`
-    },
-    age () {
-      if (this.birthyear === '' || this.birthmonth === '' || this.birthday === '') {
-        return ''
-      }
-      var birthDate = new Date(this.birthyear, this.birthmonth, this.birthday)
-      var diffMs = Date.now() - birthDate.getTime()
-      var ageDt = new Date(diffMs)
-      return Math.abs(ageDt.getUTCFullYear() - 1970)
-    }
-  },
-
-  watch: {
-    name (newVal, oldVal) {
-      console.log(newVal, oldVal)
+  methods: {
+    format () {
+      this.formattedName = this.name.split(' ').join('-')
     }
   }
 }
