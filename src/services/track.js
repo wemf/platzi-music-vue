4f0e189100683b9f4a35c29e8cf550d88ba5071f
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-08 11:57:40 -0500

    add services

---
+++ src/services/track.js
@@ -0,0 +1,13 @@
+import platziMusicService from './platziMusicService'
+
+const trackService = {}
+
+trackService.search = function (q) {
+  const type = 'track'
+
+  return platziMusicService.get('/search', {
+    params: { q, type }
+  }).then(res => res.data)
+}
+
+export default trackService
