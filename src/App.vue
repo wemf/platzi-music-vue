7e16408e98f6a3c552982f0d7390b48900413b98
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-14 17:05:10 -0500

    comunicacion componentes padres a hijos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   #app
     pm-header
-    section.section
+
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
       nav.nav.has-shadow
         .container
           .field.has-addons
@@ -13,48 +15,57 @@
               )
             .control
               a.button.is-info.is-large(@click="search") Buscar
+            .control
               a.button.is-danger.is-large &times;
       .container
         p
           small {{ searchMessage }}
 
       .container.results
-        .columns
-          .column(v-for="t in tracks")
-            | {{ t.name }} - {{ t.artists[0].name}}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:track="t")
+
     pm-footer
 </template>
 
 <script>
-import trackService from './services/track'
-import PmHeader from './components/layout/Header.vue'
-import PmFooter from './components/layout/Footer.vue'
+import trackService from '@/services/track'
+import PmFooter from '@/components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
+
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false
     }
   },
 
-  components: {
-    PmFooter,
-    PmHeader
-  },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader },
 
   computed: {
     searchMessage () {
       return `Encontrados. ${this.tracks.length}`
     }
   },
+
   methods: {
     search () {
       if (!this.searchQuery) { return }
-      trackService.search(this.searchQuery).then(res => {
-        this.tracks = res.tracks.items
-      })
+
+      this.isLoading = true
+
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+          this.isLoading = false
+        })
     }
   }
 }
