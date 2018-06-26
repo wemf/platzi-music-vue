c1be7d66644e517a0c7f7ba813fb2d493c0191e5
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-26 17:02:05 -0500

    sin terminar vuex clase 44

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import msToMm from '@/filters/ms-to-mm'
 import blur from '@/directives/blur'
 
 import routes from '@/routes'
-
+import store from '@/store'
 import EventBus from '@/plugins/event-bus'
 
 Vue.use(VueRouter)
@@ -21,5 +21,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 })
