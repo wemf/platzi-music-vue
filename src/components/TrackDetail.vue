55a334db56da064b44ac289bb636f88703cc5bca
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-06-26 15:29:36 -0500

    mixins 37

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,19 +1,46 @@
 <template lang="pug">
   .container
     .columns
-      .column.is-5.is-offset-4
-        pm-track(:track="track")
+      .column.is-3.has-text-centered
+        figure.media-left
+          p.image
+            img(:src="track.album.images[0].url")
+          p &nbsp;
+          p
+            a.button.is-primary.is-large
+              span.icon(@click="selectTrack") ▶
+
+      .column.is-8
+        .panel
+          .panel-heading
+            h1.title Cancion: {{ track.name }} | Album: {{ track.album.name }}
+          .panel-block
+            .article-media
+              .media-content
+                .content
+                  ul(v-for="(v, k) in track")
+                    li
+                      strong {{ k }}:&nbsp;
+                      span {{ v }}
+
+              nav.nav-level
+                .level-left
+                  a.level-item
 </template>
+
 <script>
-import PmTrack from '@/components/Track.vue'
+import trackMixin from '@/mixins/track'
 import trackService from '@/services/track'
+
 export default {
-  components: { PmTrack },
+  mixins: [trackMixin],
+
   data () {
     return {
       track: {}
     }
   },
+
   created () {
     const id = this.$route.params.id
 
@@ -24,9 +51,9 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-.columns {
-  margin: 20px;
-}
-</style>
 
+<style lang="scss">
+  .column {
+    margin: 20px;
+  }
+</style>
